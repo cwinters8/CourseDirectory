2419749ca20fbd1b30fd2be3e6949c4b55c72473
cwinters8 <clwinters8@gmail.com>
2019-02-13 17:15:24 -0800

    added basename for the routers

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ import NotFound from './NotFound';
 import Featured from './Featured';
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/CourseDirectory">
     <div className="container">
       <Header />
       <Switch>
